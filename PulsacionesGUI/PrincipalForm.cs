a25872db0d25fc1cb9fc87977caa46b3efac0a65
Eduardo23o1 <62672629+Eduardo23o1@users.noreply.github.com>
2020-06-03 15:45:16 -0500

    Agregar archivos de proyecto.

---
+++ PulsacionesGUI/PrincipalForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PulsacionesGUI
+{
+    public partial class PrincipalForm : Form
+    {
+        public PrincipalForm()
+        {
+            InitializeComponent();
+        }
+
+        private void registrarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ingresarPersonaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RegistrarPersonasForm registrarPersonasForm = new RegistrarPersonasForm();
+            registrarPersonasForm.Show();
+        }
+
+        private void consultarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ConsultarForm consultarForm = new ConsultarForm();
+            consultarForm.Show();
+        }
+
+        private void consultarToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            ConsultarTodoForm consultarTodoForm = new ConsultarTodoForm();
+            consultarTodoForm.Show();
+        }
+    }
+}
